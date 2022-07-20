780a530c4ea2915be322b70962e4cd8cfbcb159b
Ayyappan <ayyappan.rajesh16@gmail.com>
2022-07-20 13:16:14 -0400

    uploading fillincrements

---
+++ Week 1/Task 2/FillIncrements/FillIncrements/main.asm
@@ -0,0 +1,49 @@
+;
+; FillIncrements.asm
+;
+; Created: 7/20/2022 12:54:10 PM
+; Author : Ayyappan
+;
+
+
+;
+; WriteToLocation.asm
+;
+; Created: 7/14/2022 12:24:47 AM
+; Author : Ayyappan
+;
+
+; Replace with your application code
+.INCLUDE "M328PBDEF.INC"
+RJMP START
+NOP
+
+.dseg; Data segment
+.org 0x100;start from location 0x100
+loc : .BYTE 0x100 ;start from location 0x100
+
+.cseg
+
+start:
+	CLR r1
+	LDI r16, 0
+	ldi YH, high(loc)
+	ldi YL, low(loc)
+
+	ST Y,R16
+
+FillIncrements:
+    ldi Yh,high(loc)
+    ldi Yl,low(loc)
+	ldi R17,0
+	ldi R16,0
+	st Y+,R16
+	FillLoop:
+	INC R17
+	st y+,R17
+
+	DEC R16
+	brne FillLoop
+	ret
+		
+
