5f08e0a3d838f1c108ad0dac8f6cdc3ee7844a78
Abhishek Bhattiprolu <saiabhishekbhattiprolu@gmail.com>
2022-07-25 11:06:27 -0400

    Add files via upload

--- main.asm
+++ main.asm
@@ -6,34 +6,36 @@
 ;
 
 
-  .equ	SIZE	= 199		;data block size
-  .equ	TABLE_L	=$100		;Low SRAM address of first data element
-  .equ	TABLE_H	=$00		;High SRAM address of first data element
-                                    
-  rjmp	RESET		;Reset Handle
+  .cseg
+  .org 0x00
 
 
-  .def	A=r13		;first value to be compared
-  .def	B=r14		;second value to be compared
-  .def	cnt2=r15		;inner loop counter
-  .def	cnt1=r16		;outer loop counter
-  .def	endL=r17		;end of data array low address
-  .def	endH=r18		;end of data array high address
 
-  ; ***CODE GOES HERE AYYAPPAN***
+  ldi r19,$09
+  outer_loop:
+  ldi r22, 00	;loading pointer a with value 0100
+  ldi r23, 01
+  ldi r18, $09   ; loading counter value 9 for inner loop
 
-     bubble:
-                                    	mov	ZL,endL
-                                    	mov	ZH,endH		;init Z pointer
-                                    	mov	cnt2,cnt1	;counter2 <- counter1
-                                    i_loop:	ld	A,Z		;get first byte, A (n)
-                                    	ld	B,-Z		;decrement Z and get second byte, B (n-1)
-                                    	cp	A,B		;compare A with B
-                                    	brlo	L1		;if A not lower 
-                                    	st	Z,A		;    store swapped
-                                    	std	Z+1,B
-                                    L1:	dec	cnt2
-                                    	brne	i_loop		;end inner loop
-                                    	dec	cnt1
-                                    	brne	bubble		;end outer loop		
-                                    	ret
+  inner_loop:
+  ld r16,a+	; loading the value pointed by a to r16 and increment a
+  ld r17,a
+  cp r17,r16	; comparing the first and second elements
+  brlt swp		; swap if less tham
+
+  reset:
+  dec r18
+  brne inner_loop	;if branch != 0 to innerloop
+  dec r19
+  brne outer_loop
+
+  end:
+  rjmp end
+
+
+  swp:
+  st a,r17		;swap
+  st -a,r17		; decremented the a value
+  inc r22
+  rjmp reset
+
